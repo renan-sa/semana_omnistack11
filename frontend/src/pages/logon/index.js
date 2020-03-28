4fb1b197a61fb9bad7b302cfedef5aefbcf1783e
renansa <renansa2017.estudo@gmail.com>
2020-03-28 12:58:22 -0300

    Finalizado o frontend

--- frontend/src/pages/logon/index.js
+++ frontend/src/pages/logon/index.js
@@ -1,23 +1,47 @@
-import React from 'react';
-import { Link } from 'react-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
 import './style.css';
 import LogoImg from '../../assets/logo.svg';
 import HeroesImg from '../../assets/heroes.png'; 
+import api from '../../services/api';
 
 function Logon () {
+    const [ id, setId ] = useState('');
+
+    const history = useHistory();
+
+    async function handleLogon (e) {
+        e.preventDefault();
+
+        try {
+            const response = await api.post('sessions', { id });
+
+            localStorage.setItem('ongId', id);
+            localStorage.setItem('ongName', response.data.name);
+
+            history.push('/profile');
+        } catch (err) {
+            alert('Erro no logon, tente novamente.');
+        }
+    }
+
     return (
         <div className="logon-container">
             <section className="form">
                 <img src={LogoImg} alt="Be the Hero"/>
 
-                <form>
+                <form onSubmit={ handleLogon }>
                     <h1>Faça seu Logon</h1>
 
-                    <input placeholder="Sua ID"/>
+                    <input 
+                        placeholder="Sua ID"
+                        value={ id }
+                        onChange={ e => setId(e.target.value) }
+                    />
                     <button className="button" type="submit">Entrar</button>
 
-                    <Link to="/register">
+                    <Link class="back-link" to="/register">
                         <FiLogIn size={16} color="#E02041"/>
                         Não tenho cadastro
                     </Link>
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Logon from './pages/logon';
 import Register from './pages/register';
+import Profile from './pages/profile';
+import NewIncident from './pages/newIncident';
 
 function Routes () {
     return (
@@ -9,6 +11,8 @@ function Routes () {
             <Switch>
                 <Route path="/" exact component={Logon}/>
                 <Route path="/register" component={Register}/>
+                <Route path="/profile" component={Profile}/>
+                <Route path="/incidents/new" component={NewIncident}/>
             </Switch>
         </BrowserRouter>
     );
